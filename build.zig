cfe8e56886301281293f330c3127956b4b8e3e75
David Rubin <daviru007@icloud.com>
2024-01-16 22:38:44 -0800

    update to new build system

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
 
-    const mod = b.addModule("tracer", .{ .source_file = .{ .path = "src/mod.zig" } });
+    const mod = b.addModule("tracer", .{ .root_source_file = .{ .path = "src/mod.zig" } });
 
     addTest(b, target, mode, mod, 0);
     addTest(b, target, mode, mod, 1);
@@ -15,7 +15,7 @@ pub fn build(b: *std.Build) void {
     _ = test_step;
 }
 
-fn addTest(b: *std.Build, target: std.zig.CrossTarget, mode: std.builtin.Mode, mod: *std.build.Module, comptime backend: u8) void {
+fn addTest(b: *std.Build, target: std.Build.ResolvedTarget, mode: std.builtin.Mode, mod: *std.Build.Module, comptime backend: u8) void {
     const options = b.addOptions();
     options.addOption(usize, "src_file_trimlen", std.fs.path.dirname(std.fs.path.dirname(@src().file).?).?.len);
     options.addOption(u8, "backend", backend);
@@ -27,7 +27,7 @@ fn addTest(b: *std.Build, target: std.zig.CrossTarget, mode: std.builtin.Mode, m
         .optimize = mode,
     });
     exe.linkLibC();
-    exe.addModule("tracer", mod);
-    exe.addOptions("build_options", options);
+    exe.root_module.addImport("tracer", mod);
+    exe.root_module.addOptions("build_options", options);
     b.installArtifact(exe);
 }
